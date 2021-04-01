c04b1e6fb9d593b36ca78c8cac7623df47cfccf2
Eugene Kulesha <eugene.kulesha@nanoporetech.com>
2021-04-01 17:05:17 +0100

    the stats for the downloads page

---
+++ sql/patch-20210401.sql
@@ -0,0 +1,7 @@
+insert into meta(meta_key, meta_value) values ('stats.covid_domains', 47), ('stats.covid_structures', 9279);
+insert into meta(meta_key, meta_value) values ('library.representative.scop_fa_represeq_lib20210330.fa', 'scop family representative domain sequences');
+insert into meta(meta_key, meta_value) values ('library.representative.scop_sf_represeq_lib20210330.fa', 'scop superfamily representative domain sequences');
+insert into meta(meta_key, meta_value) values ('library.clustering.scop_fa_represeq_lib20210330.95clust', '');
+insert into meta(meta_key, meta_value) values ('library.clustering.scop_fa_represeq_lib20210330.40clust', '');
+insert into meta(meta_key, meta_value) values ('library.clustering.scop_sf_represeq_lib20210330.95clust', '');
+insert into meta(meta_key, meta_value) values ('library.clustering.scop_sf_represeq_lib20210330.40clust', '');
